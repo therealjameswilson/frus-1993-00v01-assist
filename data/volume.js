76a9b2ab65dcc320f534b38d1512fa865e9bab30
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 07:03:29 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,15 @@
+window.FRUS_VOLUME = {
+  "index": 28,
+  "doc_id": "frus1993-00v01",
+  "title": "1993\u20132000, Volume I, Foundations of Foreign Policy",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1993-00v01",
+  "repo_slug": "frus-1993-00v01-assist",
+  "title_ascii": "1993-2000, Volume I, Foundations of Foreign Policy",
+  "pages_url": "https://therealjameswilson.github.io/frus-1993-00v01-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1993-00v01-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=Foundations+OR+Policy&from=1993&to=2000&sort=relevance&perColl=25&perPage=50",
+  "companion": [
+    "Foundations of Foreign Policy source map",
+    "https://therealjameswilson.github.io/Clinton-Foundations/"
+  ]
+};
